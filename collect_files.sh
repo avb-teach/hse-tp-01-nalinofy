53241a28ca123f25d1a8d898f25478f034b360c6
nalinofy <nastya_pankrashkina@mail.ru>
2025-04-29 00:41:27 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-
 chmod +x collect_files.sh
-
 input_dir="$1"
-output_dir="\$2"
+output_dir="$2"
 
-# Check if input directory exists
-if [ ! -d "$input_dir" ]; then
-    exit 1
-fi
 
 mkdir -p "$output_dir"
-
 copy_files() {
     local src="$1"
-    local dest="\$2"
+    local dest="$2"
     
     find "$src" -type f | while read -r file; do
         filename=$(basename "$file")
@@ -33,5 +26,4 @@ copy_files() {
         fi
     done
 }
-
 copy_files "$input_dir" "$output_dir"
